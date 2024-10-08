61f83f8f62df8176f6845283072e71d2cd22c316
Thomas Manley <tmanley1985@gmail.com>
2024-10-08 19:25:24 -0400

    Added the service for the nyt service

--- app/Services/NYT/NYTService.php
+++ app/Services/NYT/NYTService.php
@@ -5,5 +5,8 @@ declare(strict_types=1);
 namespace App\Services\NYT;
 
 class NYTService {
-    
+    public function __construct(
+        private readonly string $apiKey,
+        private readonly string $uri,
+    ) {}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Services\NYT\NYTService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/1/nyt/best-sellers', function (Request $request, NYTService $nytService) {
+    return response()->json('JHI');
+});
