48df7947c9cdd40155bb320edb4c1c5308e3efcc
Thomas Manley <tmanley1985@gmail.com>
2024-10-09 06:55:22 -0400

    Removed comments

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,4 @@
 use App\Http\Controllers\SearchBooks;
 use Illuminate\Support\Facades\Route;
 
-// TODO: Put the service behind an interface and bind this to a config value.
 Route::get('/1/nyt/best-sellers', SearchBooks::class);
